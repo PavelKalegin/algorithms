5952319ee83cdf21ae0e7566cf3a3e935059cd31
pavel <pavelkalegin@gmail.com>
2017-10-03 22:42:25 +0200

    Priority queue implementing

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args){
         //Factorials.run();
         //GreedyAlg.run();
-        Haffman.run();
+        //Haffman.run();
+        PriorityQueue.run();
     }
 }
